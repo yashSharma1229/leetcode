7de7d1cbf875a75979c119c1fd1f5e5b8a7b0682
Yash Sharma <146512371+yashSharma1229@users.noreply.github.com>
2026-08-21 23:50:29 +0530

    Time: 4 ms (14.19%), Space: 22.1 MB (59.98%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int>st;
+       
+        int n= asteroids.size();
+                for(int i=0;i<n;i++){
+                    if(asteroids[i]>0){
+                        st.push(asteroids[i]);
+                    }
+                    else{
+                        while(!st.empty() && st.top()<abs(asteroids[i]) && st.top()>0){
+                            st.pop();
+                        }
+                        if(!st.empty() && st.top()>0 && st.top()==abs(asteroids[i])){
+                            st.pop();
+                        }
+                        else if(st.empty() || st.top()<0){
+                            st.push(asteroids[i]);
+                        }
+                    }
+                    
+
+        
+        }
+         vector<int> ans(st.size());
+         for (int i = st.size() - 1; i >= 0; i--) {
+            ans[i] = st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
